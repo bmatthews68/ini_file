de9a7f822ab64339175a91b69e9bcd8d128932d3
Brian Matthews <brian@btmatthews.com>
2015-05-13 07:04:28 +0100

    Work in progress transitioning to vendoring of dependencies

--- metadata.rb
+++ metadata.rb
@@ -4,4 +4,4 @@ maintainer_email 'brian@btmatthews.com'
 license          'All rights reserved'
 description      'Creates or updates a .ini file'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.0.0'
+version          '1.1.0'
--- providers/default.rb
+++ providers/default.rb
@@ -1,3 +1,5 @@
+require Helper
+
 action :create do
   converge_by("Create or overwrite #{new_resource}") do
     create_inifile
@@ -8,7 +10,7 @@ end
 action :create_if_missing do
   if !exists?
     converge_by("Create #{new_resource} because it does not exist") do
-      create_inifile
+      create_inifile(new_resource.path, new_resource.settings)
       new_resource.updated_by_last_action(true)
     end
   end
@@ -17,12 +19,12 @@ end
 action :update do
   if exists?
     converge_by("Update #{current_resource} because it already exists") do
-      update_inifile
+      update_inifile(new_resource.path, new_resource.settings) 
       new_resource.updated_by_last_action(true)
     end
   else
     converge_by("Create #{new_resource} because it does not exist") do
-      create_inifile
+      create_inifile(new_resource.path, new_resource.settings)
       new_resource.updated_by_last_action(true)
     end
   end
@@ -35,27 +37,6 @@ end
 def load_current_resource
   @current_resource = Chef::Resource::IniFile.new(new_resource.name)
   @current_resource.path(new_resource.path)
-end
-
-def create_inifile
-  inifile = ::IniFile.new
-  inifile.filename = new_resource.path
-  new_resource.settings.each do |section, settings|
-    settings.each do |name, value|
-      inifile[section][name] = value
-    end
-  end
-  inifile.write
-end
-
-def update_inifile
-  inifile = ::IniFile.load(@current_resource.path)
-  new_resource.settings.each do |section, settings|
-    settings.each do |name, value|
-      inifile[section][name] = value
-    end
-  end
-  inifile.write
 end
 
 def exists?
